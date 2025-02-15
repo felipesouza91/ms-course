b57ad71cd2c0ffee6d1b3c1257c9cbf8825a9382
Felipe Souza Santana <felipedb91@gmail.com>
2025-02-14 22:06:51 -0300

    feat: add ribbon load balance

--- hr-worker/src/main/java/dev/fsantana/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/dev/fsantana/hrworker/resources/WorkerResource.java
@@ -2,6 +2,9 @@ package dev.fsantana.hrworker.resources;
 
 import dev.fsantana.hrworker.entities.Worker;
 import dev.fsantana.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Objects;
 
+
 @RestController
 @RequestMapping("/workers")
 public class WorkerResource {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkerResource.class.getName());
+
+    private final Environment env;
+
     private final WorkerRepository workerRepository;
 
-    public WorkerResource(WorkerRepository workerRepository) {
+    public WorkerResource(Environment env, WorkerRepository workerRepository) {
+        this.env = env;
         this.workerRepository = workerRepository;
     }
 
@@ -28,10 +37,12 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
         Worker worker = workerRepository.findById(id).orElse(null);
         if(Objects.isNull(worker)) {
             return ResponseEntity.badRequest().build();
         }
+        LOGGER.info("Request in Port:" + env.getProperty("server.port"));
         return ResponseEntity.ok(worker );
     }
 }
