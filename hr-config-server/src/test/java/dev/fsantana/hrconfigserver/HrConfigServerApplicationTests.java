f279ffda244c36d5906ba5f170b426f81785ae38
Felipe Souza Santana <felipedb91@gmail.com>
2025-02-18 20:56:45 -0300

    feat: add hr config server

---
+++ hr-config-server/src/test/java/dev/fsantana/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fsantana.hrconfigserver;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HrConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
