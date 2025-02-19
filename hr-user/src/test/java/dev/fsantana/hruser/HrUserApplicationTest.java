5666d8e5e8d769063968315693e8a7d6628beced
Felipe Souza Santana <felipedb91@gmail.com>
2025-02-19 20:11:54 -0300

    chore: create hr-user

---
+++ hr-user/src/test/java/dev/fsantana/hruser/HrUserApplicationTest.java
@@ -0,0 +1,12 @@
+package dev.fsantana.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HrUserApplicationTest {
+
+    @Test
+    void name() {
+    }
+}
