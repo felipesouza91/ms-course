747f9470b0c04719f00a99041e4640ef74cb1b2d
Felipe Souza Santana <felipedb91@gmail.com>
2025-02-19 21:33:22 -0300

    chore: create hr-oauth project

---
+++ hr-oauth/src/test/java/dev/fsantana/hroauth/HrOauthApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.fsantana.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HrOauthApplicationTest {
+
+    @Test
+    void name() {
+
+    }
+}
