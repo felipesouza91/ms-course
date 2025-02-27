0f8188a727c7eb878fe9cdbd6c83da6cd7876c65
Felipe Souza Santana <felipedb91@gmail.com>
2025-02-26 21:27:22 -0300

    ci: create docker files to build imagens for each project

---
+++ scripts/init_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE db_hr_worker;
+GRANT ALL PRIVILEGES ON DATABASE db_hr_worker TO hr_db_user;
+
+CREATE DATABASE db_hr_user;
+GRANT ALL PRIVILEGES ON DATABASE db_hr_user TO hr_db_user;
